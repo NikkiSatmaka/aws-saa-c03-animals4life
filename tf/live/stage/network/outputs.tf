17d90707a54911888a8d411d17e937b1c3faca42
Nikki Satmaka <30800020+NikkiSatmaka@users.noreply.github.com>
2024-10-20 21:48:41 +0700

    remove ip cidrs from vpc output

--- tf/live/stage/network/outputs.tf
+++ tf/live/stage/network/outputs.tf
@@ -2,23 +2,3 @@ output "vpc_id" {
   description = "ID of the VPC"
   value       = module.vpc.vpc_id
 }
-
-output "reserved_subnet_cidrs" {
-  description = "The IP CIDRs for reserved subnet"
-  value       = module.vpc.reserved_subnet_cidrs
-}
-
-output "db_subnet_cidrs" {
-  description = "The IP CIDRs for db subnet"
-  value       = module.vpc.db_subnet_cidrs
-}
-
-output "app_subnet_cidrs" {
-  description = "The IP CIDRs for app subnet"
-  value       = module.vpc.app_subnet_cidrs
-}
-
-output "web_subnet_cidrs" {
-  description = "The IP CIDRs for web subnet"
-  value       = module.vpc.web_subnet_cidrs
-}
